05f62b4345ba67ef75e85039ff295e3e230c4292
Alina <alina.blank@gmail.com>
2022-02-07 16:26:24 +0200

    Add project files.

---
+++ DateCounter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace Task5
+{
+    static class DateCounter
+    {
+        public static int DaysToBirthday(DateTime birthday)
+        {
+            DateTime today = DateTime.Today;
+            int day = birthday.Day;
+            int month = birthday.Month;
+            int year = DateTime.Today.Year;
+            if (today.Month > month)
+            {
+                year += 1;
+            }
+            DateTime nextBithday = new DateTime(year, month, day);
+            TimeSpan time = nextBithday - today;
+            return time.Days;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Task5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime bithday = Convert.ToDateTime(Console.ReadLine());
+            Console.WriteLine("Days to bithday: {0}", DateCounter.DaysToBirthday(bithday));
+        }
+    }
+}
